47f6aca3c2769ec69adf7cc028fb586879c642ca
Aromee Kim <aromeekim@Aromees-MacBook-Pro.local>
2021-05-29 23:33:11 -0700

    initial commit

---
+++ main_app/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('items/add', views.ItemCreate.as_view(), name='items_add'),
+    path('items/<int:pk>/delete/', views.ItemDelete.as_view(), name='home'),
+]
---
+++ main_app/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.views.generic import ListView, CreateView, DeleteView
+from .models import Item
+
+# Create your views here.
+def home(request):
+    items = Item.objects.all()
+    return render(request, 'home.html', { 'items' : items })
+
+class ItemCreate(CreateView): 
+    model = Item
+    fields = '__all__'
+    success_url = '/'
+
+class ItemDelete(DeleteView): 
+    model = Item
+    success_url = '/'
